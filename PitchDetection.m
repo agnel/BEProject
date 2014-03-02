0fd5260d01d6a52f9628b66305ed47947e55e175
Agnel Waghela <agnelwaghela@gmail.com>
2014-03-02 20:34:53 +0000

    Changes to PitchDetection

--- PitchDetection.m
+++ PitchDetection.m
@@ -8,8 +8,8 @@ function [f0] = PitchDetection(c, Fs)
 %   OUTPUTS:
 %   @param  type: @vector       f0:              Estimated Pitch
 %   Author: Agnel Waghela.
-%   Version: 1.0
-%   Date: 1 March, 2014
+%   Version: 1.1
+%   Date: 2nd March, 2014
 %   Dependencies: None
 
 %% Searching for Maximum between 2ms(500Hz) and 20ms(50Hz)
@@ -17,5 +17,5 @@ ms2 = floor(Fs*0.002); % 2ms
 ms20 = floor(Fs*0.02); % 20ms
 
 %% find the maximum and the Index of the maximum
-[~,idx] = max(abs(c(ms2:ms20)));
-f0 = Fs/(ms2+idx-1);
+[~,idx] = max(abs(c(ms2:ms20))); % idx is the index of the peak and ~ is used because we do not need the magnitude of the peak
+f0 = idx;
